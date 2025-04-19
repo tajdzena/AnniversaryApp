e18700b0adcf31c85baa03d2759ca6c24419fe0f
tajdzena <vracevictijana@gmail.com>
2025-04-20 01:15:43 +0200

    Dodavanje fonta, prvi import slike

--- app/src/main/java/com/example/anniversaryapp/MainActivity.kt
+++ app/src/main/java/com/example/anniversaryapp/MainActivity.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.anniversaryapp.ui.theme.AnniversaryAppTheme
+import com.example.anniversaryapp.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.animation.core.*
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +35,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AnniversaryAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Tijana",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AnniversaryApp()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Tijana",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AnniversaryApp(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnniversaryAppTheme {
-        Greeting("Android")
+    val scale = remember { Animatable(1f) }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text(
+            text = "Klikni na srce!",
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = 20.sp,
+            color = Color.DarkGray
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.pixel_heart),
+            contentDescription = "Pixel Heart",
+            modifier = Modifier
+                .size(512.dp)
+                .scale(scale.value)
+        )
     }
 }
+
+
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    AnniversaryAppTheme {
+//        Greeting("Android")
+//    }
+//}
